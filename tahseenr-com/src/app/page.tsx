1a6596c3921b80cc028d87fd96c026f075476686
Tahseen Rayhan <seen.rayhan@gmail.com>
2025-06-12 01:00:28 -0400

    testing toggle

--- tahseenr-com/src/app/page.tsx
+++ tahseenr-com/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import SkillToggle from "./components/SkillToggle";
 
 export default function Home() {
   return (
@@ -18,12 +19,13 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
             </code>
-            .
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
         </ol>
+
+        <SkillToggle/>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
