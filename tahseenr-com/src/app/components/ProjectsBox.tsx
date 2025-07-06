07e1351b6dec30727c08d12310a7ee7b98e8ed62
Tahseen Rayhan <seen.rayhan@gmail.com>
2025-07-06 18:59:45 -0400

    Created static projects box

--- tahseenr-com/src/app/components/ProjectsBox.tsx
+++ tahseenr-com/src/app/components/ProjectsBox.tsx
@@ -15,9 +15,9 @@ interface ProjectBoxProps {
 
 const ReturnTags = (tech_tags: string[]) => {
     return (
-        <div>
+        <div className="flex flex-row flex-wrap gap-2">
             {tech_tags.map((tag, index) => (
-                <div key={index} className="tag">
+                <div key={index} className="bg-[#54787e] p-2 rounded-xl text-white text-xs">
                     {tag}
                 </div>
             ))}
@@ -34,13 +34,18 @@ const ProjectsBox: React.FC<ProjectBoxProps> = ({project_name, description, proj
     return (
         <div className="hover:shadow-xl/20 shadow-black-500 transition-shadow duration-300 flex flex-row bg-[#22303b] align-items items-center font-poppins m-4 mt-7 rounded-xl h-auto w-3/4 p-6 px-9 gap-8">
             <a href={gh_link} target="_blank" rel="noopener noreferrer">
-            <p className="font-bold text-[20px] opacity-90">{project_name}</p>
-            <p className="font-raleway">{description}</p>
-            {ReturnTags(tech_tags)}
-            <Image
-                src={project_img}
-                alt="Company Logo"
-                className="rounded-xl size-25"/>
+                <div className="flex flex-row gap-9">
+                    <div>
+                        <p className="font-bold text-[20px] opacity-90 mb-1">{project_name}</p>
+                        <p className="font-raleway text-sm mb-3">{description}</p>
+                        {ReturnTags(tech_tags)}
+                    </div>
+                    
+                    <Image
+                        src={project_img}
+                        alt="Company Logo"
+                        className="rounded-xl size-50"/>
+                </div>
             </a>
         </div>
     );
