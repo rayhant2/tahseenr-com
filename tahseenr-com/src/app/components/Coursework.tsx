924132fc340b9086ca4493e83886d005f6b5f86b
Tahseen Rayhan <seen.rayhan@gmail.com>
2025-07-19 00:05:26 -0400

    Added education section to page

---
+++ tahseenr-com/src/app/components/Coursework.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import Image from "next/image";
+import LaurierLogo from "../../../public/laurier_logo.png";
+import WaterlooLogo from "../../../public/Uwaterloo_logo.png";
+
+const Coursework = () => {
+    return (
+        <div className="text-white p-8 font-poppins">
+            <div className="flex flex-col">
+
+                <div className="flex flex-row gap-10">
+                    <Image
+                        src={WaterlooLogo}
+                        alt="University of Waterloo"
+                        className="rounded-xl size-30"/>
+
+                    <div className="flex flex-col">
+                        <div className="flex flex-row gap-3 items-center">
+                            <p className="text-xl font-bold">University of Waterloo</p>
+                            <p className="opacity-70 text-xs md:block hidden">(2025 - 2029)</p>
+                        </div>
+                        
+                        <p className="opacity-80">Computer Science</p>
+                        <div className="mt-5 sm:block hidden">
+                            <p>Relevant courses:</p>
+                            <p className="text-sm font-style: italic font-light ml-5 mt-2 opacity-80">Designing Functional Programs, Algorithm Design, Linear Algebra, Discrete Mathematics, Calculus (1/2), Combinatorics & Optimization</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className="mt-5 sm:hidden block">
+                        <p>Relevant courses:</p>
+                        <p className="text-sm font-style: italic font-light ml-5 mt-2 opacity-80">Designing Functional Programs, Algorithm Design, Linear Algebra, Discrete Mathematics, Calculus (1/2), Combinatorics & Optimization</p>
+                </div>
+                
+            </div>
+            
+            <div className="flex w-full items-center justify-center my-15 opacity-15">
+                <div className="bg-white h-[1px] w-4/5"/>
+            </div>
+
+            <div className="flex flex-col mb-3">
+
+                <div className="flex flex-row gap-10">
+                    <Image
+                        src={LaurierLogo}
+                        alt="Wilfrid Laurier University"
+                        className="rounded-xl size-30"/>
+
+                    <div className="flex flex-col">
+                        <div className="flex flex-row gap-3 items-center">
+                            <p className="text-xl font-bold">Wilfrid Laurier University</p>
+                            <p className="opacity-70 text-xs md:block hidden">(2025 - 2029)</p>
+                        </div>
+                        
+                        <p className="opacity-80">Business Administration</p>
+                        <div className="mt-5 sm:block hidden">
+                            <p>Relevant courses:</p>
+                            <p className="text-sm font-style: italic font-light ml-5 mt-2 opacity-80">Understanding the Business Environment, Microeconomics, Macroeconomics, Financial Managment, Accounting</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className="mt-5 sm:hidden block">
+                        <p>Relevant courses:</p>
+                        <p className="text-sm font-style: italic font-light ml-5 mt-2 opacity-80">Understanding the Business Environment, Microeconomics, Macroeconomics, Financial Managment, Accounting</p>
+                </div>
+                
+            </div>
+
+        </div>
+    );
+}
+
+export default Coursework;
